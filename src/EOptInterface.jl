20f332f205be45efc8b6ab164bf7bd13c5940705
Dimitri Alston <123396563+DimitriAlston@users.noreply.github.com>
2026-04-22 06:27:45 -0400

    Update examples (#5)

--- src/EOptInterface.jl
+++ src/EOptInterface.jl
@@ -1,16 +1,31 @@
+# Copyright (c) 2025 Joseph Choi, Dimitri Alston, Pengfei Xu, Matthew Stuber,
+# and the University of Connecticut (UConn)
+# This code is licensed under the MIT license (see LICENSE.md for full details).
+################################################################################
+# EOptInterface
+# An abstraction layer for optimizing equation-oriented/acausal models
+# https://github.com/PSORLab/EOptInterface.jl
+################################################################################
+# src/EOptInterface.jl
+# The main file for EOptInterface.
+################################################################################
+
 module EOptInterface
 
-# ---- imports ----
-using ModelingToolkit
-using ModelingToolkit: t_nounits as t, D_nounits as D
+    # Imports
+    import JuMP
+    import ModelingToolkit
+    import ModelingToolkit: t_nounits as t, D_nounits as D
+    import Symbolics
+    import SymbolicUtils
 
-using JuMP
+    # Include base functions
+    include("basefuncs.jl")
 
-# ---- includes ----
-include("basefuncs.jl")
-include("userfuncs.jl")
+    # Include user functions
+    include("userfuncs.jl")
 
-# ---- exports ----
-export decision_vars, full_solutions, register_nlsystem, register_odesystem
+    # Exports
+    export decision_vars, full_solution, register_nlsystem, register_odesystem
 
-end # module
+end
